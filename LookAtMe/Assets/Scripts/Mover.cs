37a5e194bc0f2ccd8fe731898cc0aee52cb2d42e
118dg <118dg@naver.com>
2021-03-05 19:57:13 +0900

    part 4

---
+++ LookAtMe/Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Mover : MonoBehaviour
+{
+    private InputDevice _device;
+    private CharacterController _character;
+    private Vector2 _inputAxis; //where to move
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand); //input source를 그냥 하드코딩으로 넣어줄 수도 있음
+        _character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _device.TryGetFeatureValue(CommonUsages.primary2DAxis, out _inputAxis);
+        //조이스틱 움직이거나 안 움직이거나 암튼 그 좌표 가져와서 _inputAxis 변수에 넣음
+        _character.Move(motion: new Vector3(_inputAxis.x, y: 0, z: _inputAxis.y) * Time.deltaTime * 1f);
+    }
+}
---
+++ LookAtMe/Assets/Scripts/Trigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField]
+    private Transform theDoor; //location of the door
+
+    private float startX;
+    private bool isOpen; // open or close
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        startX = theDoor.position.x; //시작할 때 문 x값
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //참고로 미닫이문임. 슬라이드식.
+        if (isOpen && theDoor.position.x < startX + 1f) //문 열건데 문 x값이 (시작 x값 + 1.0)보다 작으면
+        {
+            theDoor.position += theDoor.right * Time.deltaTime; //문 옆으로 밀어서 열기!
+        }
+
+        if (!isOpen && theDoor.position.x > startX)
+        {
+            theDoor.position -= theDoor.right * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) //파라미터 other : trigger 작동시킨 것
+    {
+        //만약 어떤 key에 의해서만 문이 열리게 하고 싶고(trigger object가 작동하도록 하고 싶고)
+        //그 key를 right hand라고 한다면 XR rig/Camera offset/Righthand Controller/Cube에 Tag: key 추가하고
+        //rigidbody 추가 후 isKinematic 체크. 단, Gravity는 끄기.
+        if (other.gameObject.tag == "key")
+        {
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            isOpen = false;
+        }
+    }
+}
