80fbfebf60e96c78929e6b29c80ba95e19c31087
118dg <118dg@naver.com>
2021-03-09 13:03:39 +0900

    Part7 - Moving and Turning

--- LookAtMe/Assets/Scripts/Mover.cs
+++ LookAtMe/Assets/Scripts/Mover.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Mover : MonoBehaviour
 {
     private InputDevice _device;
     private CharacterController _character;
     private Vector2 _inputAxis; //where to move
+    private GameObject _camera;
 
     // Start is called before the first frame update
     private void Start()
     {
         _device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand); //input source를 그냥 하드코딩으로 넣어줄 수도 있음
         _character = GetComponent<CharacterController>();
+        _camera = GetComponent<XRRig>().cameraGameObject;
     }
 
     // Update is called once per frame
     private void Update()
     {
         _device.TryGetFeatureValue(CommonUsages.primary2DAxis, out _inputAxis);
-        //조이스틱 움직이거나 안 움직이거나 암튼 그 좌표 가져와서 _inputAxis 변수에 넣음
-        _character.Move(motion: new Vector3(_inputAxis.x, y: 0, z: _inputAxis.y) * Time.deltaTime * 1f);
+        //check if left joystick is being pressed. if so, sign that info to _inputAxis variable 
+
+        var inputVector = new Vector3(_inputAxis.x, Physics.gravity.y, _inputAxis.y); //direction that controller is being pushed in
+        //Physics.gravity.y => apply gravity to character controller so character can actually fall now. no more floating.
+        var inputDirection = transform.TransformDirection(inputVector); //transform inputVector to direction
+        var lookDirection = new Vector3(0, _camera.transform.eulerAngles.y, 0); //카메라로 어느 방향 보고 있는지
+        var newDirection = Quaternion.Euler(lookDirection) * inputDirection; //???????내적??????어떤 연산인지 아직 이해X
+
+        //_character.Move(motion: new Vector3(_inputAxis.x, y: 0, z: _inputAxis.y) * Time.deltaTime * 1f);
+        _character.Move(newDirection * Time.deltaTime * 1f);
+
+        //왼쪽 조이스틱을 앞으로 향하게 고정해둔 상태에서 원래는 world 좌표계 기준으로(?) 움직였는데 이제는 고개 돌리면 고개 돌린 방향으로 나아감
     }
 }
---
+++ LookAtMe/Assets/Scripts/SmoothTurning.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class SmoothTurning : MonoBehaviour
+{
+
+    private InputDevice device;
+    private Vector2 inputStick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand); //오른쪽 조이스틱은 회전(왼쪽은 이동)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputStick);
+
+        if(inputStick.x > 0.2f || inputStick.x < -0.2f) //조이스틱 조금 건들인것 말고 확실히 움직였을때만
+        {
+            Turn();    
+        }
+    }
+
+    private void Turn()
+    {
+        var rotationAmount = transform.eulerAngles.y + inputStick.x; //y축 기준 회전
+        var directionVector = new Vector3(transform.eulerAngles.x, rotationAmount, transform.eulerAngles.z); //회전한 위치값
+        transform.rotation = Quaternion.Euler(directionVector); //회전한 위치값으로 rotate
+    }
+}
